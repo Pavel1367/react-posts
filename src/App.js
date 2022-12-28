8929a1c63fe9c176d5a6970751b0b526bcd129a7
pavel <pashka1367@bk.ru>
2022-12-28 21:04:05 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useEffect } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Navbar from "./components/UI/navbar/Navbar";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Error from "./pages/Error";
+import "./styles/App.css";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/posts/:id" element={<PostPage />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/posts" element={<Posts />}></Route>
+        <Route path="*" element={<Posts/>}></Route>
+      </Routes>
     </div>
   );
 }
